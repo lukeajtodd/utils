82671f571fbb09116dbd264acd489385489f9ff7
Luke Todd <luketodd@zoho.com>
2019-03-23 21:42:51 +0000

    Fixing issue with import in contains

--- packages/class-list/jest.config.js
+++ packages/class-list/jest.config.js
@@ -4,4 +4,5 @@ module.exports = {
   ...base,
   name: '@lt-utils/class-list',
   displayName: '@lt-utils/class-list',
+  collectCoverageFrom: ['**/*.{ts}', '!**/node_modules/**'],
 };
--- packages/contains/jest.config.js
+++ packages/contains/jest.config.js
@@ -4,4 +4,5 @@ module.exports = {
   ...base,
   name: '@lt-utils/contains',
   displayName: '@lt-utils/contains',
+  collectCoverageFrom: ['<rootDir>/src/*.{ts}', '!**/node_modules/**'],
 };
