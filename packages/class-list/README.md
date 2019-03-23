208ca3e9704578486062525f51c564b322905926
Luke Todd <luketodd@zoho.com>
2019-03-23 20:35:37 +0000

    Added class list package back in

---
+++ packages/class-list/README.md
@@ -0,0 +1,8 @@
+# Typescript Starter
+
+[![Codecov Coverage](https://img.shields.io/codecov/c/github/lukeajtodd/typescript-package-starter/master.svg?style=flat-square)](https://codecov.io/gh/lukeajtodd/typescript-package-starter)
+
+- Typescript
+- TSLint
+- Prettier
+- Jest
---
+++ packages/class-list/src/index.ts
@@ -0,0 +1,50 @@
+import { contains } from '@utils/contains';
+
+/**
+ * Method that checks whether cls is present in element object. (Will set class attribute to empty if not present).
+ * @param  {Object} ele DOM element which needs to be checked
+ * @param  {Object} cls Classname is tested
+ * @return {Boolean} True if cls is present, false otherwise.
+ */
+export const hasClass = (ele: HTMLElement, cls: string) => {
+  if (!ele.hasAttribute('class')) {
+    ele.setAttribute('class', '');
+    return false;
+  }
+  return ele.getAttribute('class')!.indexOf(cls) > -1;
+};
+
+/**
+ * Method that adds a class to given element.
+ * @param  {Object} ele DOM element where class needs to be added
+ * @param  {Object} cls Classname which is to be added
+ * @return {null} nothing is returned.
+ */
+export const addClass = (ele: HTMLElement, cls: string) => {
+  if (ele.classList) {
+    if (contains(cls, ' ')) {
+      let clsArr = cls.split(' ');
+      for (let i = 0; i < clsArr.length; i++) {
+        ele.classList.add(clsArr[i]);
+      }
+    } else {
+      ele.classList.add(cls);
+    }
+  } else if (!hasClass(ele, cls)) {
+    ele.setAttribute('class', ele.getAttribute('class') + ' ' + cls);
+  }
+};
+
+/**
+ * Method that does a check to ensure that class is removed from element.
+ * @param  {Object} ele DOM element where class needs to be removed
+ * @param  {Object} cls Classname which is to be removed
+ * @return {null} Null nothing is returned.
+ */
+export const removeClass = (ele: HTMLElement, cls: string) => {
+  if (ele.classList) {
+    ele.classList.remove(cls);
+  } else if (hasClass(ele, cls)) {
+    ele.setAttribute('class', ele.getAttribute('class')!.replace(cls, ' '));
+  }
+};
